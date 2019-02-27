81875c1f4447ebe3f9cc59d3a6a688f977592373
Abetang Joseph <joeabetang@gmail.com>
2019-02-27 18:11:46 +0100

    added action column and approprate buttons

--- js/app.js
+++ js/app.js
@@ -1,5 +1,19 @@
 $(document).ready(() => {
-   
     $('#example').DataTable();
+
+    
+    
+
+    let $firstname = $('#firstname').value();
+    let $lastname = $('#lastname').value();
+    let $position = $('#position').value();
+    let $phone = $('#phone').value();
+
+    $('#form').submit((e)=>{
+        e.preventDefault();
+
+    })
+
+   
 
 });

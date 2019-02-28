2ab5c5bf664eed87cadfab6b40823637fd451536
Abetang Joseph <joeabetang@gmail.com>
2019-02-28 08:01:42 +0100

    added view functionality, post and delete working well

--- js/app.js
+++ js/app.js
@@ -1,65 +1,122 @@
+let BaseURL = 'http://127.0.0.1:3000/'
+let $tbody = $('#tbody');
+
+let list = [];
+
+//fetch all users from db
+let FetchAllEmployees = () => {
+    $.ajax({
+        type: 'GET',
+        url: `${BaseURL}employees`,
+        success: (res) => {
+            let response = '';
+            $.each(res, (i, employees) => {
+                list = res;
+                response += ` 
+            <tr>
+               <td>${employees.firstname}</td>
+               <td>${employees.lastname}</td>
+               <td>${employees.position}</td>
+               <td>${employees.gender}</td>
+               <td>${employees.phone}</td>
+               <td><span><div class="custom-control custom-checkbox">
+                   <input type="checkbox" class="custom-control-input" id="${employees.id}" value="">
+                   <label class="custom-control-label" for="${employees.id}">attended</label>
+                 </div></span></td>
+               <td><span><button class="btn btn-secondary" onclick=getUser(${employees.id}) data-toggle="modal" data-target="#employeeDetials"><i class="fas fa-eye"></i></button> <button class="btn btn-primary"><i class="fas fa-pencil-alt"></i></button> <button onclick ="removeEmployee(${employees.id})" class="btn btn-danger"><i class="fas fa-trash-alt"></i></button></span></td>
+            </tr>
+             `
+
+            })
+            $tbody.html(response);
+            $('#example').DataTable();
+
+
+        },
+        error: () => {
+            const Toast = Swal.mixin({
+                toast: true,
+                position: 'top-end',
+                showConfirmButton: false,
+                timer: 4000
+            });
+
+            Toast.fire({
+                type: 'error',
+                title: 'content could not be retrieved'
+            })
+        }
+    })
+}
+
+//end fetch all
+
+const getUser = (id) => {
+    const user = list.find(element => element.id === id);
+    console.log(user);
+    $("#user").html(
+        `<div>
+        <span><p>First Name : ${user.firstname}</p><p>Last Name : ${user.firstname}</p></span>
+            
+        </div>`
+    )
+}
+
+
+let removeEmployee = (emp_id)=>{
+    Swal.fire({
+        title: 'Are you sure?',
+        text: "You won't be able to revert this!",
+        type: 'warning',
+        showCancelButton: true,
+        confirmButtonColor: '#3085d6',
+        cancelButtonColor: '#d33',
+        confirmButtonText: 'Yes, delete it!'
+      }).then((remove) => {
+        if (remove.value) {
+
+            $.ajax({
+                type: 'DELETE',
+                url: `${BaseURL}employees/${emp_id}`,
+                success: () => {
+                    const Toast = Swal.mixin({
+                        toast: true,
+                        position: 'top-end',
+                        showConfirmButton: false,
+                        timer: 4000
+                    });
+                    FetchAllEmployees();
+    
+                    Toast.fire({
+                        type: 'success',
+                        title: 'employee record deleted'
+                    })
+                    
+                },
+                error: () => {
+                    const Toast = Swal.mixin({
+                        toast: true,
+                        position: 'top-end',
+                        showConfirmButton: false,
+                        timer: 4000
+                    });
+                    FetchAllEmployees();
+    
+                    Toast.fire({
+                        type: 'error',
+                        title: 'record could not be deleted'
+                    })
+                }
+            })
+
+        }
+      })
+}
 $(document).ready(() => {
-    let BaseURL = 'http://127.0.0.1:3000/'
-
-    let $tbody = $('#tbody'); //target html table
-
-    //fetch all users from db
-    let FetchAllEmployees = () => {
-        $.ajax({
-            type: 'GET',
-            url: `${BaseURL}employees`,
-            success: (res) => {
-                // console.log(res);
-
-                let response = '';
-                $.each(res, (i, employees) => {
-                    response += ` 
-                <tr>
-                   <td>${employees.firstname}</td>
-                   <td>${employees.lastname}</td>
-                   <td>${employees.position}</td>
-                   <td>${employees.gender}</td>
-                   <td>${employees.phone}</td>
-                   <td><span><div class="custom-control custom-checkbox">
-                       <input type="checkbox" class="custom-control-input" id="${employees.id}">
-                       <label class="custom-control-label" for="${employees.id}">attended</label>
-                     </div></span></td>
-                   <td><span><button class="btn btn-secondary" data-toggle="modal" data-target="#employeeDetials"><i class="fas fa-eye"></i></button> <button class="btn btn-primary"><i class="fas fa-pencil-alt"></i></button> <button id="${employees.id}" class="btn btn-danger"><i class="fas fa-trash-alt"></i></button></span></td>
-                </tr>
-                 `
-
-                })
-                $tbody.append(response);
-                $('#example').DataTable();
-
-
-            },
-            error: () => {
-                const Toast = Swal.mixin({
-                    toast: true,
-                    position: 'top-end',
-                    showConfirmButton: false,
-                    timer: 4000
-                });
-
-                Toast.fire({
-                    type: 'error',
-                    title: 'content could not be retrieved'
-                })
-            }
-        })
-        //end fetch all.
-    }
 
     FetchAllEmployees();
 
-
-
     //post to db
-
-
-
-
     $('#saveButton').on('click', () => {
         $firstname = $('#firstname').val();
         $lastname = $('#lastname').val();
@@ -94,26 +151,27 @@ $(document).ready(() => {
                 url: `${BaseURL}employees`,
                 data: payload,
                 success: (employees) => {
-                    console.log(employees);
-                    let response = '';
-                    response += ` 
-                    <tr>
-                       <td>${employees.firstname}</td>
-                       <td>${employees.lastname}</td>
-                       <td>${employees.position}</td>
-                       <td>${employees.gender}</td>
-                       <td>${employees.phone}</td>
-                       <td><span><div class="custom-control custom-checkbox">
-                           <input type="checkbox" class="custom-control-input" id="${employees.id}">
-                           <label class="custom-control-label" for="${employees.id}">attended</label>
-                         </div></span></td>
-                       <td><span><button class="btn btn-secondary" data-toggle="modal" data-target="#employeeDetials"><i class="fas fa-eye"></i></button> <button class="btn btn-primary"><i class="fas fa-pencil-alt"></i></button> <button id="${employees.id}" class="btn btn-danger"><i class="fas fa-trash-alt"></i></button></span></td>
-                    </tr>
-                     `
+                   
+                    // let response = '';
+                    // response += ` 
+                    // <tr>
+                    //    <td>${employees.firstname}</td>
+                    //    <td>${employees.lastname}</td>
+                    //    <td>${employees.position}</td>
+                    //    <td>${employees.gender}</td>
+                    //    <td>${employees.phone}</td>
+                    //    <td><span><div class="custom-control custom-checkbox">
+                    //        <input type="checkbox" class="custom-control-input" id="${employees.id}">
+                    //        <label class="custom-control-label" for="${employees.id}">attended</label>
+                    //      </div></span></td>
+                    //    <td><span><button class="btn btn-secondary" data-toggle="modal" data-target="#employeeDetials"><i class="fas fa-eye"></i></button> <button class="btn btn-primary"><i class="fas fa-pencil-alt"></i></button> <button onclick = "removeEmployee(${employees.id})" class="btn btn-danger"><i class="fas fa-trash-alt"></i></button></span></td>
+                    // </tr>
+                    //  `
 
-                    $tbody.append(response);
-
-                    $('#example').DataTable();
+                    // $tbody.append(response);
+                    
+                    FetchAllEmployees();
+                     
 
                     //alert if employee add successful
                     const Toast = Swal.mixin({
@@ -147,7 +205,7 @@ $(document).ready(() => {
             })
         }
 
-
+       
 
 
     })
@@ -161,11 +219,6 @@ $(document).ready(() => {
 
 
 
-
-    $('#form').submit((e) => {
-        e.preventDefault();
-
-    })
 
 
 
